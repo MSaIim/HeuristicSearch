ca7af790bea7af9548320be842a88d74396bdf93
Mohammad Salim <mfsalim27@gmail.com>
2017-01-23 23:47:19 -0500

    Continue highway logic

--- Cell.py
+++ Cell.py
@@ -6,6 +6,13 @@ class Type(Enum):
 	HARD = 2
 	BLOCKED = 3
 
+class Direction(Enum):
+	NONE = 1
+	UP = 2
+	DOWN = 3
+	LEFT = 4
+	RIGHT = 5
+
 # The individual Cell
 class Cell(object):
 	def __init__(self):
@@ -14,9 +21,10 @@ class Cell(object):
 
 # Point class to hold coordinates
 class Point(object):
-	def __init__(self, x, y):
+	def __init__(self, x, y, direction = Direction.NONE):
 		self.x = x
 		self.y = y
+		self.direction = direction
 
 	def __eq__(self, other):
 		return self.x == other.x and self.y == other.y
--- Grid.py
+++ Grid.py
@@ -1,5 +1,6 @@
 from random import randrange, uniform
 from Cell import Type
+from Cell import Direction
 from Cell import Cell
 from Cell import Point
 
@@ -7,8 +8,8 @@ class Grid(object):
 	ROWS = 120
 	COLUMNS = 160
 	NUMPOINTS = 8
-	NUMHIGHWAYS = 4
-
+	NUMHIGHWAYS = 1
+	highwayTurns = []
 
 	def __init__(self):
 		self.initialize()
@@ -73,36 +74,124 @@ class Grid(object):
 	# Create highway by going 20 cells in a direction
 	def createHighway(self, coord):
 		points = []
-		rows = self.ROWS-1
-		columns = self.COLUMNS-1
-		hLength = 20
+		done = False
+		probability = uniform(0, 1)
 
-		# GO DOWN
-		if coord.x == 0:
-			for index in range(hLength):
-				if self.cells[index][coord.y].isHighway == False:
-					points.append(Point(index, coord.y))
-		# GO UP
-		elif coord.x == rows:
-			for index in reversed(range(rows-hLength+1, self.ROWS)):
-				if self.cells[index][coord.y].isHighway == False:
-					points.append(Point(index, coord.y))
-		# GO RIGHT
+		# Initial Placement
+		if coord.x == self.ROWS - 1:
+			points = self.setHighwayCells(coord, points, Direction.UP)
+		elif coord.x == 0:
+			points = self.setHighwayCells(coord, points, Direction.DOWN)
+		elif coord.y == self.COLUMNS - 1:
+			points = self.setHighwayCells(coord, points, Direction.LEFT)
 		elif coord.y == 0:
-			for index in range(hLength):
-				if self.cells[coord.x][index].isHighway == False:
-					points.append(Point(coord.x, index))
-		# GO LEFT
-		elif coord.y == columns:
-			for index in reversed(range(columns-hLength+1, self.COLUMNS)):
-				if self.cells[coord.x][index].isHighway == False:
-					points.append(Point(coord.x, index))
+			points = self.setHighwayCells(coord, points, Direction.RIGHT)
 
-		# Check size of highway
+		# Continue Highway
 		if len(points) == 20:
+			while(True):
+				# Check if all options exhausted
+				if len(self.highwayTurns) == 3:
+					del points[:]
+					del self.highwayTurns[:]
+					break
+
+				# Get last index and coordinate
+				index = len(points) - 1
+				coord = points[index]
+
+				# Continue the highway
+				points = self.setHighwayDirection(coord, points, probability)
+
+				# Check if highway is done
+				if len(points) >= 100 and self.isBoundaryPoint(points[len(points) - 1]) == True:
+		 			done = True
+		 			break
+		 		# Something went wrong, delete the points that were just added
+				elif len(points) % 20 != 0:
+					del points[index:]
+					self.highwayTurns.append(probability)	# Add the probability to try a different path
+
+		# Highway complete
+		if done == True:
 			for point in points:
 				self.cells[point.x][point.y].isHighway = True
 			return True
 
 		# Something went wrong
 		return False
+
+
+	# Select a direction for the highway to move to
+	def setHighwayDirection(self, coord, points, probability):
+		# Check if a direction failed. Go a different route
+		if(len(self.highwayTurns) > 0):
+			if self.highwayTurns[len(self.highwayTurns)-1] < 0.59:
+				probability = 0.70	# Try going DOWN or LEFT
+			elif self.highwayTurns[len(self.highwayTurns)-1] > 0.59 and self.highwayTurns[len(self.highwayTurns)-1] < 0.79:
+				probability = 0.90	# Try going UP or RIGHT
+			else:
+				probability = 0.50	# Try going in the same direction
+
+		# Continue in same direction
+		if probability < 0.59:
+			if coord.direction == Direction.UP:
+				points = self.setHighwayCells(coord, points, Direction.UP)
+			elif coord.direction == Direction.DOWN:
+				points = self.setHighwayCells(coord, points, Direction.DOWN)
+			elif coord.direction == Direction.LEFT:
+				points = self.setHighwayCells(coord, points, Direction.LEFT)
+			elif coord.direction == Direction.RIGHT:
+				points = self.setHighwayCells(coord, points, Direction.RIGHT)
+
+		# Continue in perpendicular (DOWN or LEFT)
+		elif probability < 0.79:
+			if coord.direction == Direction.UP or coord.direction == Direction.DOWN:
+				points = self.setHighwayCells(coord, points, Direction.LEFT)
+			elif coord.direction == Direction.LEFT or coord.direction == Direction.RIGHT:
+				points = self.setHighwayCells(coord, points, Direction.DOWN)
+
+		# Continue in perpendicular (UP or RIGHT)
+		elif probability < 1:
+			if coord.direction == Direction.UP or coord.direction == Direction.DOWN:
+				points = self.setHighwayCells(coord, points, Direction.RIGHT)
+			elif coord.direction == Direction.LEFT or coord.direction == Direction.RIGHT:
+				points = self.setHighwayCells(coord, points, Direction.UP)
+
+		return points
+
+
+	# Set the cells to be flagged as highways
+	def setHighwayCells(self, cell, points, direction):
+		highwayLength = 20
+
+		# GO UP
+		if direction == Direction.UP:
+			for index in range(cell.x-highwayLength, cell.x+1):
+				if(index > 0 and self.cells[index][cell.y].isHighway == False):
+					points.append(Point(index, cell.y, Direction.UP))
+
+		# GO DOWN
+		elif direction == Direction.DOWN:
+			for index in range(cell.x, cell.x+highwayLength):
+				if(index < self.ROWS and self.cells[index][cell.y].isHighway == False):
+					points.append(Point(index, cell.y, Direction.DOWN))
+
+		# GO LEFT
+		elif direction == Direction.LEFT:
+			for index in range(cell.y-highwayLength, cell.y+1):
+				if(index >= 0 and self.cells[cell.x][index].isHighway == False):
+					points.append(Point(cell.x, index, Direction.LEFT))
+
+		# GO RIGHT
+		elif direction == Direction.RIGHT:
+			for index in range(cell.y, cell.y+highwayLength):
+				if(index < self.COLUMNS and self.cells[cell.x][index].isHighway == False):
+					points.append(Point(cell.x, index, Direction.RIGHT))
+
+		return points
+
+
+	# Check if point is a boundary coordinate
+	def isBoundaryPoint(self, point):
+		return point.x == 0 or point.x == 119 or point.y == 0 or point.y == 159
