e1cf27698001764a1000990c861c9d9ecdaeb469
Mohammad Salim <mfsalim27@gmail.com>
2017-02-07 23:29:17 -0500

    Add units to Excel Columns

--- Tests/ExcelLists.py
+++ Tests/ExcelLists.py
@@ -1,16 +1,16 @@
 # Headers (String,, startRow, startColumn)
 headers = [
 	("Start", 1, 0), ("Goal", 1, 1), ("Average", 13, 0),
-	("AStar",          0, 3),  ("Time", 1, 3),  ("Path Length", 1, 4),  ("Nodes Expanded", 1, 5),
-	("Uniform Cost",   0, 7),  ("Time", 1, 7),  ("Path Length", 1, 8),  ("Nodes Expanded", 1, 9),
-	("Manhattan 1.25", 0, 11), ("Time", 1, 11), ("Path Length", 1, 12), ("Nodes Expanded", 1, 13),
-	("Manhattan 2.00", 0, 15), ("Time", 1, 15), ("Path Length", 1, 16), ("Nodes Expanded", 1, 17),
-	("Euclidean 1.25", 0, 19), ("Time", 1, 19), ("Path Length", 1, 20), ("Nodes Expanded", 1, 21),
-	("Euclidean 2.00", 0, 23), ("Time", 1, 23), ("Path Length", 1, 24), ("Nodes Expanded", 1, 25),
-	("Diagonal 1.25",  0, 27), ("Time", 1, 27), ("Path Length", 1, 28), ("Nodes Expanded", 1, 29),
-	("Diagonal 2.00",  0, 31), ("Time", 1, 31), ("Path Length", 1, 32), ("Nodes Expanded", 1, 33),
-	("Chebyshev 1.25", 0, 35), ("Time", 1, 35), ("Path Length", 1, 36), ("Nodes Expanded", 1, 37),
-	("Chebyshev 2.00", 0, 39), ("Time", 1, 39), ("Path Length", 1, 40), ("Nodes Expanded", 1, 41)
+	("AStar",          0, 3),  ("Time (ms)", 1, 3),  ("Path Length (cost)", 1, 4),  ("Nodes Expanded", 1, 5),
+	("Uniform Cost",   0, 7),  ("Time (ms)", 1, 7),  ("Path Length (cost)", 1, 8),  ("Nodes Expanded", 1, 9),
+	("Manhattan 1.25", 0, 11), ("Time (ms)", 1, 11), ("Path Length (cost)", 1, 12), ("Nodes Expanded", 1, 13),
+	("Manhattan 2.00", 0, 15), ("Time (ms)", 1, 15), ("Path Length (cost)", 1, 16), ("Nodes Expanded", 1, 17),
+	("Euclidean 1.25", 0, 19), ("Time (ms)", 1, 19), ("Path Length (cost)", 1, 20), ("Nodes Expanded", 1, 21),
+	("Euclidean 2.00", 0, 23), ("Time (ms)", 1, 23), ("Path Length (cost)", 1, 24), ("Nodes Expanded", 1, 25),
+	("Diagonal 1.25",  0, 27), ("Time (ms)", 1, 27), ("Path Length (cost)", 1, 28), ("Nodes Expanded", 1, 29),
+	("Diagonal 2.00",  0, 31), ("Time (ms)", 1, 31), ("Path Length (cost)", 1, 32), ("Nodes Expanded", 1, 33),
+	("Chebyshev 1.25", 0, 35), ("Time (ms)", 1, 35), ("Path Length (cost)", 1, 36), ("Nodes Expanded", 1, 37),
+	("Chebyshev 2.00", 0, 39), ("Time (ms)", 1, 39), ("Path Length (cost)", 1, 40), ("Nodes Expanded", 1, 41)
 ]
 
 avgHeader = [("Average", 13, 0), ("Average", 28, 0), ("Average", 43, 0), ("Average", 58, 0), ("Average", 73, 0)]
